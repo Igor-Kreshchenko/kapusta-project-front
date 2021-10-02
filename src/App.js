0946ac99af5c4aa97fb3e7918d53b65cbe82a757
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-10-03 00:42:22 +0300

    Добавляет основные рауты

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { Switch, Route } from "react-router-dom";
+
+// Расскоментировать, поправить пути , если нужно. Вставить в рауты
+
+// const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+// const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
+// const TransactionsPage = lazy(() => import("./pages/TransactionsPage"));
+// const StatisticsPage = lazy(() => import("./pages/HomePage/StatisticsPage"));
 
 function App() {
   return (
     <div className="App">
-      <h1>Just do it!</h1>
+      <Suspense fallback={<p>...Loading</p>}>
+        <Switch>
+          <Route path="/login">{"LoginPage"}</Route>
+
+          <Route path="/register">{"RegisterPage"}</Route>
+
+          <Route path="/transactions">{"TransactionsPage"}</Route>
+
+          <Route path="/statistics">{"StatisticsPage"}</Route>
+        </Switch>
+      </Suspense>
     </div>
   );
 }
