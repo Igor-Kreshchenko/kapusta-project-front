557b648ea8ac69cf8ddad9f86f450251fc565b7a
Nina Motorna <nina.motorna@gmail.com>
2021-10-03 17:58:17 +0200

    adds buttonText as props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.scss";
 
-const Button = ({ Button }) => {
+const Button = ({ Button, buttonText }) => {
   return (
     <button type="button" onClick={Button} className={styles.button}>
-      ВОЙТИ
+      {buttonText}
     </button>
   );
 };
 
 Button.propTypes = {
   Button: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
 };
 
 export default Button;
