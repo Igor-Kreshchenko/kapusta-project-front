7b72a81aa335367d77f6354b2663bbd516370068
Nina Motorna <nina.motorna@gmail.com>
2021-10-03 18:06:48 +0200

    buttonHandler prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,16 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.scss";
 
-const Button = ({ Button, buttonText }) => {
+const Button = ({ buttonHandler, buttonText }) => {
   return (
-    <button type="button" onClick={Button} className={styles.button}>
+    <button type="button" onClick={buttonHandler} className={styles.button}>
       {buttonText}
     </button>
   );
 };
 
 Button.propTypes = {
-  Button: PropTypes.func.isRequired,
+  buttonHandler: PropTypes.func.isRequired,
   buttonText: PropTypes.string.isRequired,
 };
 
