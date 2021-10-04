31b581ee3db8fa2507ab04415bab5c20a4a71eb4
Klem26 <Kankina26@gmail.com>
2021-10-05 01:49:18 +0300

    add component CategoriesList,  UserSummary, ToggleStatistics,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
+import CategoriesList from "./components/CategoriesList/CategoriesList";
 
 // Расскоментировать, поправить пути , если нужно. Вставить в рауты
 
@@ -7,10 +8,11 @@ import { Switch, Route } from "react-router-dom";
 // const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 // const TransactionsPage = lazy(() => import("./pages/TransactionsPage"));
 // const StatisticsPage = lazy(() => import("./pages/HomePage/StatisticsPage"));
-
+const StatisticsPage = lazy(() => import("./pages/StatisticsPage/StatisticsPage"));
 function App() {
   return (
     <div className="App">
+      {/* <CategoriesList/> */}
       <Suspense fallback={<p>...Loading</p>}>
         <Switch>
           <Route path="/login">{"LoginPage"}</Route>
@@ -19,7 +21,9 @@ function App() {
 
           <Route path="/transactions">{"TransactionsPage"}</Route>
 
-          <Route path="/statistics">{"StatisticsPage"}</Route>
+          <Route path="/statistics">
+            <StatisticsPage />
+          </Route>
         </Switch>
       </Suspense>
     </div>
