53b34aba50bbb164a4d664e4aaf1e1d176407017
deniskodak <denis.kodak@gmail.com>
2021-10-06 08:55:45 +0300

    добавляет в редакс секцию транзакций: редюсеры, операции, акшены. Устанавливает аксиос

---
+++ src/redux/transactions/index.js
@@ -0,0 +1,3 @@
+export { default as transactionsActions } from "./transactionsActions";
+export { default as transactionsReducer } from "./transactionsReducer";
+export { default as transactionOps } from "./transactionsOps";
