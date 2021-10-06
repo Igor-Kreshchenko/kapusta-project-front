90c74aad588ed51fc415750ba5ec3db46d10c41f
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-10-06 17:38:04 +0300

    промжуточній коммит

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import CategoriesList from "./components/CategoriesList/CategoriesList";
-
+import Notification from "./components/ZeroBalanceNotification";
 // Расскоментировать. Исправить путь импорта, если нужно. Вставить компонент в раут
 
 // const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 // const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 // const TransactionsPage = lazy(() => import("./pages/TransactionsPage"));
 // const StatisticsPage = lazy(() => import("./pages/HomePage/StatisticsPage"));
-const StatisticsPage = lazy(() => import("./pages/StatisticsPage/StatisticsPage"));
+const StatisticsPage = lazy(() =>
+  import("./pages/StatisticsPage/StatisticsPage")
+);
 
 function App() {
   return (
     <div className="App">
       {/* <CategoriesList/> */}
+      <Notification />
       <Suspense fallback={<p>...Loading</p>}>
         <Switch>
           <Route path="/login">{"LoginPage"}</Route>
