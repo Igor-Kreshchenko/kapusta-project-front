69138dcde342ec6669cb00f186406b9d18232492
YanaHorbachova <maya69877@gmail.com>
2021-10-10 23:34:36 +0300

    добавлено получение месяца и года на странице Отчетов

--- src/components/CurrentPeriod/CurrentPeriod.js
+++ src/components/CurrentPeriod/CurrentPeriod.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
+import transactionsOps from '../../redux/transactions/transactionsOps';
 import DatePicker, { registerLocale } from "react-datepicker";
 import "../../../node_modules/react-datepicker/dist/react-datepicker.css";
 import styles from './CurrentPeriod.module.scss'
@@ -7,10 +9,14 @@ import { format } from 'date-fns'
 registerLocale("ru", ru);
 
 const CurrentDatePicker = () => {
+    const dispatch = useDispatch();
     const [startDate, setStartDate] = useState(new Date());
- 
-    const currentDate = format(new Date(startDate), 'MM/yyyy')
-    console.log(currentDate)
+    const month = format(new Date(startDate), 'MM' )
+    const year = format(new Date(startDate), 'yyyy' )
+
+    // useEffect(() => {
+    //   dispatch(transactionsOps.getAllTransactionByMonth({ month, year }));
+    // }, [month, dispatch, year]);
 
     return (
         <div className={styles.DatePickerConteiner}>
