f202291318a335e084d1db4b7f58e1d20270cc52
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-10-10 12:16:54 +0300

    Дописан выпадающий список

--- src/components/TransactionsExpForm/TransactionsExpForm.js
+++ src/components/TransactionsExpForm/TransactionsExpForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import MyDatePicker from "../MyDatePicker";
-
 import styles from "./TransactionsExpForm.module.scss";
+import CategoryList from "../CategoryExpensesList/CategoryList";
 
 const TransactionsExpForm = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -13,9 +13,147 @@ const TransactionsExpForm = () => {
         onChange={(date) => setStartDate(date)}
       />
       <div className={styles.form_input}>
-        <input type="text" placeholder="Описание товара" />
-        <input type="text" placeholder="Категория товара" />
-        <input type="text" placeholder="0,00" />
+        <input
+          className={styles.form_input_description}
+          type="text"
+          placeholder="Описание товара"
+        />
+        <div className={styles.form_input_category_parent}>
+          <input
+            className={styles.form_input_category}
+            id="datalist_input"
+            type="text"
+            placeholder="Категория товара"
+          />
+          <ul className={styles.form_category_list} data-value="list">
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="food"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Продукты
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="health"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Здоровье
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="alcohol"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Алкоголь
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="entertainment"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Развлечения
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="allForHouse"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Всё для дома
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="technic"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Техника
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="utilities"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Коммуналка, связь
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="hobby"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Спорт, хобби
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="education"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Образование
+              </label>
+            </li>
+            <li className={styles.form_option}>
+              <label>
+                <input
+                  hidden
+                  value="other"
+                  type="radio"
+                  name="exp_category"
+                  className={styles.radiobutton}
+                />
+                Прочее
+              </label>
+            </li>
+          </ul>
+          <span className={styles.list_arrow}></span>
+        </div>
+        <input
+          className={styles.form_input_amount}
+          type="text"
+          placeholder="0,00"
+        />
       </div>
 
       <div className={styles.form_btn}>
