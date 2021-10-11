35a9d18d6bcdce5198609e328ea59347cba6dd09
Klem26 <Kankina26@gmail.com>
2021-10-11 14:46:16 +0300

    clean component

--- src/components/ToggleStatistics/Income.js
+++ src/components/ToggleStatistics/Income.js
@@ -1,44 +1,26 @@
 import React from "react";
-import  css from "../CategoriesList/CategoriesList.module.scss"
-import sprite from "../../images/icons/sprite_categories.svg";
+import CategoriesList from "../CategoriesList";
 
-const Income = () => {
+const singleIncome = [
+    {
+  _id: "0000000000",
+  date: "20.02.2021",
+  category: "ЗП",
+  amount: 100,
+  description: "вернули долг",
+    },
+      {
+  _id: "0000000009",
+  date: "20.02.2021",
+  category: "Дополнительные доходы",
+  amount: 100,
+  description: "вернули долг",
+    },
+];
+const Income = ( ) => {
     return (
-        <div className={css.container}>
-          
-            <ul className={css.list}>
-        
-                <li className={css.item}>
-                    <p className={css.text}>5000</p>
-                    <div className={css.svg_wrapper}>
-                        <svg
-                            className={css.icon}
-                            width="58"
-                            height="58"
-                        >
-                            <use xlinkHref={`${sprite}#icon-Продукты`} />
-                        </svg>
-                    </div>
-                    <h3 className={css.title}>ЗП</h3>
-                </li>
-                <li className={css.item}>
-
-                    <p className={css.text}>5000</p>
-                    <div className={css.svg_wrapper}>
-                        <svg
-                            className={css.icon}
-                            width="58"
-                            height="58"
-                        >
-                            <use xlinkHref={`${sprite}#icon-Продукты`} />
-                        </svg>
-                    </div>
-                    <h3 className={css.title}>Прочее</h3>
-                </li>
-            </ul>
-        </div>
-     
-        
+          <CategoriesList
+            obj={ singleIncome}/>
     )
 };
 export default Income;
