ba517c8bc98bc054a7900b57c546ec497a017919
Oleksandra Shelest <shelest.oleksandra.88@gmail.com>
2021-10-12 16:01:06 +0300

    Вписала модалку в страницу транзакций

--- src/pages/TransactionsPage/TransactionsPage.js
+++ src/pages/TransactionsPage/TransactionsPage.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import BalancePanelHome from "../../components/BalancePanel/BalancePanelHome";
 import TransactionsExpenses from "../../components/TransactionsExpenses";
 import TransactionsIncome from "../../components/TransactionsIncome";
 import ContainerMain from "../../components/Container/ContainerMain";
-
+import storePersistor from "../../redux/store.js";
+import ZeroBalanceNotification from "../../components/ZerBalanceNotification/ZeroBalanceNotification";
 import "./TransactionsPage.scss";
 
 const TransactionsPage = () => {
+  const [isModal, setIsToggleModal] = useState(true);
+  const balance = storePersistor.store.getState().transactions.balance;
+
+  const numberExpenses =
+    storePersistor.store.getState().transactions.expenses.length;
+  const onClose = () => {
+    setIsToggleModal(!isModal);
+  };
+
   return (
     <ContainerMain>
       <BalancePanelHome />
+
+      {isModal && numberExpenses === 0 && balance === 0 && (
+        <ZeroBalanceNotification onClose={onClose} />
+      )}
 
       <Tabs>
         <TabList>
