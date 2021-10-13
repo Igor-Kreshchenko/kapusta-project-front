51be43b15700134c0b6ffa26f78c936c7f6ebcd8
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-10-13 17:04:36 +0300

    Added expense and income delete logic

--- src/components/TransactionsIncome/TransactionsIncome.js
+++ src/components/TransactionsIncome/TransactionsIncome.js
@@ -2,19 +2,21 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import TransactionMonthSummary from "../TransactionMonthSummary";
 import transactionsOps from "../../redux/transactions/transactionsOps";
-// import TransactionsIncForm from "../TransactionsIncForm";
 
 import styles from "./TransactionsIncome.module.scss";
 
 const TransactionsIncome = () => {
   const dispatch = useDispatch();
-  const onDeleteIncomes = (type = "expenses", id) => {
-    dispatch(transactionsOps.deleteTransaction(type, id));
+  const onDeleteIncomes = async (id) => {
+    await dispatch(
+      transactionsOps.deleteTransaction({
+        type: "income",
+        id,
+      })
+    );
   };
   return (
     <>
-      {/* <TransactionsIncForm /> */}
-
       <div className={styles.main}>
         <div className={styles.table}>
           <div className={styles.table_head}>
@@ -33,16 +35,6 @@ const TransactionsIncome = () => {
 
                 <span>ЗП</span>
                 <span className={styles.table_income}>20 000.00 грн.</span>
-                <button
-                  type="button"
-                  onClick={() => onDeleteIncomes()}
-                  className={styles.table_item_btn}></button>
-              </li>
-              <li className={styles.table_item}>
-                <span>05.09.2019</span>
-                <span>% на остаток на карте</span>
-                <span>Доп. доход</span>
-                <span className={styles.table_income}>500.00 грн.</span>
                 <button
                   type="button"
                   onClick={() => onDeleteIncomes()}
